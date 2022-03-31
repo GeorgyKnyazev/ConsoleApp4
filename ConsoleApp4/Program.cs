60e7e8b0b4ac7ab1e7f9a8a848ee99d55046384e
Георгий <@MSI>
2022-03-31 09:01:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ярослав";
+            string surname = "Князев";
+            int Age = 4;
+            bool maleGender = true;
+            double heightInMeters = 1.1;
+            char firstLetterOfName = 'Я';
+            bool isHeanAdult = false;
+            string eyeColor = "brawn";
+            bool doesHeTolk = true;
+            bool doesHeWalk = true;
+            
+            
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
